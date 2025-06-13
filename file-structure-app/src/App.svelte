0d495772c4602e3bb10b443303b049f271e79eef
Fidel Thomet <git@ft0.ch>
2025-06-13 14:41:31 +0200

    wip: binning

--- file-structure-app/src/App.svelte
+++ file-structure-app/src/App.svelte
@@ -3,11 +3,18 @@
   import BasicSmallDots from './lib/BasicSmallDots.svelte'
   import Calendar from './lib/Calendar.svelte'
   import Binning from './lib/Binning.svelte'
+  import BinningCollapsed from './lib/BinningCollapsed.svelte'
 
-  const visualisations = { Basic, BasicSmallDots, Calendar, Binning } as const
+  const visualisations = {
+    Basic,
+    BasicSmallDots,
+    Calendar,
+    Binning,
+    BinningCollapsed
+  } as const
   type VisualisationName = keyof typeof visualisations
   const visualisationNames = Object.keys(visualisations) as VisualisationName[]
-  let selected: VisualisationName = 'Binning'
+  let selected: VisualisationName = 'BinningCollapsed'
 </script>
 
 <main>
