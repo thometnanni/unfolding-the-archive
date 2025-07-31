e98f7ae26e3335d674b1062dd5e03e9c1684e304
Giacomo Nanni <gn.nanni@gmail.com>
2025-07-31 10:07:44 +0200

    sorted projects

--- prototypes/index/src/main.js
+++ prototypes/index/src/main.js
@@ -9,19 +9,17 @@ const prototypes = [
 ]
 
 const projects = [
-  {
-    value: 'TP_255_Serpentine_Gallery_Pavilion',
-    label: 'TP 255 Serpentine Gallery Pavilion'
-  },
-  { value: 'TP_261_Markt_Hall', label: 'TP 261 Markt Hall' },
-  { value: 'TP_377_Boijmans', label: 'TP 377 Boijmans' },
   { value: 'TP_010_VPRO', label: 'TP 010 VPRO' },
   { value: 'TP_015_WoZoCo_s', label: "TP 015 WoZoCo's" },
   { value: 'TP_028_Silodam', label: 'TP 028 Silodam' },
   { value: 'TP_065_Expo_2000', label: 'TP 065 Expo 2000' },
+  { value: 'TP_065_EXPO_2000_Hannover', label: 'TP 065 Expo 2000 Hannover' },
   { value: 'TP_072_Flight_Forum', label: 'TP 072 Flight Forum' },
-  { value: 'TP_170_Eyebeam_New_York', label: 'TP 170 Eyebeam New York' }
-]
+  { value: 'TP_170_Eyebeam_New_York', label: 'TP 170 Eyebeam New York' },
+  { value: 'TP_255_Serpentine_Gallery_Pavilion', label: 'TP 255 Serpentine Gallery Pavilion' },
+  { value: 'TP_261_Markt_Hall', label: 'TP 261 Markt Hall' },
+  { value: 'TP_377_Boijmans', label: 'TP 377 Boijmans' }
+];
 
 function populateSelect(el, items) {
   items.forEach(({ value, label }) => {
